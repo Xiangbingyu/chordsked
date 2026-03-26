b1c282ad805e027c9e1b1454c95e024a3f934693
Stephen <Stephen4Sheng@gmail.com>
2026-03-26 20:11:36 +0800

    #misc 提交demo启动脚本

--- prototypes/chord-sked-prototype2/start.sh
+++ prototypes/chord-sked-prototype2/start.sh
@@ -1,28 +1,23 @@
 #!/bin/zsh
-set -e  
+set -e
 
 IMAGE_NAME="chordsked"
 CONTAINER_NAME="chordsked-app"
 HOST_PORT=3000
-CONTAINER_PORT=3000
+CONTAINER_PORT=80         
 NODE_VERSION="22.22.1"
 
-echo "清理旧容器..."
 docker rm -f "$CONTAINER_NAME" 2>/dev/null || true
 
-
-echo "安装依赖..."
-docker run --privileged --rm \
+docker run --privileged  --rm \
   -v "$(pwd)":/app \
   -w /app \
   node:$NODE_VERSION \
-  npm install --legacy-peer-deps
+  sh -c "npm install --legacy-peer-deps && npm run build"
 
-docker build -t "$IMAGE_NAME" .
-
-echo "启动容器..."
 docker run --privileged -d \
   --name "$CONTAINER_NAME" \
   --restart unless-stopped \
   -p "$HOST_PORT":"$CONTAINER_PORT" \
-  "$IMAGE_NAME"
+  -v "$(pwd)/dist":/usr/share/nginx/html:ro \
+  nginx:alpine
